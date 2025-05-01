783d59507569e845ff8fa22315c2479c9b93030c
muazaqdas <muazaqdas_mc20a7_32@dtu.ac.in>
2025-05-01 22:11:05 +0530

    feat: create constants and Transaction Model

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,10 +1,11 @@
 export default class Transaction {
-    constructor({ id, description, amount, paidBy, splits, createdAt, groupId, note }) {
+    constructor({ id, description, amount, paidBy, splits, splitType, createdAt, groupId, note }) {
       this.id = id;
       this.description = description;
       this.amount = amount;
       this.paidBy = paidBy; // array of {personId, amount}
       this.splits = splits; // array of { personId, amount }
+      this.splitType = splitType,
       this.createdAt = createdAt || new Date().toISOString();
       this.groupId = groupId,
       this.note = note
